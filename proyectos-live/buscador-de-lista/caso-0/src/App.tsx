eb728b0cff1abcaa1fbfa8069a844cb9d2ce67b4
Miguel González <mgx.gon@gmail.com>
2024-10-22 06:22:34 -0600

    buscador-de-lista caso 0 solution

--- proyectos-live/buscador-de-lista/caso-0/src/App.tsx
+++ proyectos-live/buscador-de-lista/caso-0/src/App.tsx
@@ -7,23 +7,38 @@ import api from "./api";
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    api.search(query);
+    const fetchProducts = async () => {
+      setLoading(true);
+      const productsData = await api.search(query);
+
+      setProducts(productsData);
+      setLoading(false);
+    };
+
+    fetchProducts();
   }, [query]);
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
-            <span>$ {product.price}</span>
-          </li>
-        ))}
+        {loading ? (
+          <h2>Loading products</h2>
+        ) : (
+          <>
+            {products.map((product) => (
+              <li key={product.id} className={product.price < 100 ? "sale" : ""}>
+                <h4>{product.title}</h4>
+                <p>{product.description}</p>
+                <span>$ {product.price}</span>
+              </li>
+            ))}
+          </>
+        )}
       </ul>
     </main>
   );
