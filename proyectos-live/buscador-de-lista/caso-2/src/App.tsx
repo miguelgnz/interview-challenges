f0d1b93d0eaa6bcca6a56b3836cbc5b5233866f7
Miguel González <mgx.gon@gmail.com>
2024-10-22 20:41:53 -0600

    buscador-de-lista caso 2 solution

--- proyectos-live/buscador-de-lista/caso-2/src/App.tsx
+++ proyectos-live/buscador-de-lista/caso-2/src/App.tsx
@@ -1,10 +1,10 @@
-import type {Product} from "./types";
+import type { Product } from "./types";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState, memo } from "react";
 
 import api from "./api";
 
-function Recommended() {
+const Recommended = memo(function Recommended() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
@@ -28,23 +28,90 @@ function Recommended() {
       </ul>
     </main>
   );
-}
+});
+
+//Custom useDebounce hook
+const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [delay, value]);
+
+  return debouncedValue;
+};
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [favoriteProducts, setFavoriteProducts] = useState<Product[]>(() => {
+    const localStrg = localStorage.getItem("products");
+
+    if (localStrg) {
+      return JSON.parse(localStrg);
+    }
+
+    return [];
+  });
+
   const [query, setQuery] = useState<string>("");
 
+  const debouncedQuery = useDebounce(query, 300);
+
   useEffect(() => {
-    api.search(query).then(setProducts);
-  }, [query]);
+    if (favoriteProducts.length > 0) {
+      setProducts(favoriteProducts);
+    } else {
+      api.search(debouncedQuery).then((products) => {
+        setProducts(products);
+      });
+    }
+  }, [debouncedQuery, favoriteProducts]);
+
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(favoriteProducts));
+  }, [favoriteProducts]);
+
+  const handleOnClickProduct = (id: number) => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prod) => {
+        if (prod.id === id) {
+          return {
+            ...prod,
+            favorite: !prod.favorite,
+          };
+        }
+
+        return prod;
+      });
+    });
+
+    setFavoriteProducts(products);
+  };
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      <input
+        name="text"
+        placeholder="tv"
+        type="text"
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <ul>
         {products.map((product) => (
-          <li key={product.id}>
+          <li
+            key={product.id}
+            className={product.favorite ? "fav" : ""}
+            onClick={() => {
+              handleOnClickProduct(product.id);
+            }}
+          >
             <h4>{product.title}</h4>
             <p>{product.description}</p>
             <span>$ {product.price}</span>
