e7a78d5e859068c4e8ed076a0ba0847e23d3bc21
Miguel González <mgx.gon@gmail.com>
2024-10-25 15:51:31 -0600

    wordle caso 1 solution

--- proyectos-live/wordle/caso-1/README.md
+++ proyectos-live/wordle/caso-1/README.md
@@ -1,13 +1,13 @@
 # Ejercicios de entrevista
 
 ## Nivel 1
-- [ ] Al apretar enter cuando gané debería reiniciarse el juego.
+- [X] Al apretar enter cuando gané debería reiniciarse el juego.
 
 ## Nivel 2
-- [ ] Solucionar el error de linter en `handleKeyDown`.
+- [X] Solucionar el error de linter en `handleKeyDown`.
 
 ## Nivel 3
-- [ ] Agregar la clase `present` si la letra está presente en la respuesta pero en posición incorrecta.
+- [X] Agregar la clase `present` si la letra está presente en la respuesta pero en posición incorrecta.
 
 ## Extra
-- [ ] Traerse la palabra con el helper de `api`.
+- [X] Traerse la palabra con el helper de `api`.
--- proyectos-live/wordle/caso-1/src/App.tsx
+++ proyectos-live/wordle/caso-1/src/App.tsx
@@ -1,58 +1,79 @@
-import {useEffect, useState} from "react";
+import { useCallback, useEffect, useState } from "react";
+import api from "./api";
 
 function App() {
-  const answer = "RIGHT";
+  // const answer = "RIGHT";
+
+  const [answer, setAnswer] = useState("");
   const [turn, setTurn] = useState<number>(0);
   const [status, setStatus] = useState<"playing" | "finished">("playing");
   const [words, setWords] = useState<string[][]>(() =>
-    Array.from({length: 6}, () => new Array(5).fill("")),
+    Array.from({ length: 6 }, () => new Array(5).fill(""))
   );
 
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (status === "playing") {
-      switch (event.key) {
-        case "Enter": {
-          if (words[turn].some((letter) => letter === "")) {
+  useEffect(() => {
+    api.word.random().then((data) => {
+      setAnswer(data);
+    });
+  }, []);
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (status === "playing") {
+        switch (event.key) {
+          case "Enter": {
+            if (words[turn].some((letter) => letter === "")) {
+              return;
+            }
+
+            if (words[turn].join("") === answer) {
+              setStatus("finished");
+              // return;
+            }
+
+            setTurn((turn) => turn + 1);
+
             return;
           }
+          case "Backspace": {
+            let firstEmptyIndex = words[turn].findIndex(
+              (letter) => letter === ""
+            );
 
-          if (words[turn].join("") === answer) {
-            setStatus("finished");
-          }
+            if (firstEmptyIndex === -1) {
+              firstEmptyIndex = words[turn].length;
+            }
 
-          setTurn((turn) => turn + 1);
-
-          return;
-        }
-        case "Backspace": {
-          let firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
-
-          if (firstEmptyIndex === -1) {
-            firstEmptyIndex = words[turn].length;
-          }
-
-          words[turn][firstEmptyIndex - 1] = "";
-
-          setWords(words.slice());
-
-          return;
-        }
-        default: {
-          if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
-            const firstEmptyIndex = words[turn].findIndex((letter) => letter === "");
-
-            if (firstEmptyIndex === -1) return;
-
-            words[turn][firstEmptyIndex] = event.key.toUpperCase();
+            words[turn][firstEmptyIndex - 1] = "";
 
             setWords(words.slice());
 
             return;
           }
+          default: {
+            if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
+              const firstEmptyIndex = words[turn].findIndex(
+                (letter) => letter === ""
+              );
+
+              if (firstEmptyIndex === -1) return;
+
+              words[turn][firstEmptyIndex] = event.key.toUpperCase();
+
+              setWords(words.slice());
+
+              return;
+            }
+          }
         }
+      } else if (event.key === "Enter" && status === "finished") {
+        setWords(Array.from({ length: 6 }, () => new Array(5).fill("")));
+        setTurn(0);
+        setStatus("playing");
       }
-    }
-  };
+    },
+    [status, turn, words, answer]
+  );
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
@@ -65,9 +86,24 @@ function App() {
       {words.map((word, wordIndex) => (
         <section className="word">
           {word.map((letter, letterIndex) => {
-            const isCorrect = letter && wordIndex < turn && letter === answer[letterIndex];
+            const isCorrect =
+              letter && wordIndex < turn && letter === answer[letterIndex];
 
-            return <article className={`letter ${isCorrect && "correct"}`}>{letter}</article>;
+            const isPresent =
+              letter &&
+              wordIndex < turn &&
+              letter !== answer[letterIndex] &&
+              answer.includes(letter);
+
+            return (
+              <article
+                className={`letter ${isCorrect && "correct"} ${
+                  isPresent && "present"
+                }`}
+              >
+                {letter}
+              </article>
+            );
           })}
         </section>
       ))}
